9374515243fec43471eebfa0a11b886eaac9f68b
r-gg <gajaninrastko@gmail.com>
2024-03-29 23:33:17 +0100

    Add initial impl. Extend readme.

---
+++ async_server.py
@@ -0,0 +1,456 @@
+# Copyright 2020 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Flower server."""
+
+import functools
+import threading
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+import timeit
+from logging import DEBUG, INFO
+from typing import Dict, List, Optional, Tuple, Union
+from time import sleep
+
+from flwr.common import (
+    Code,
+    DisconnectRes,
+    EvaluateIns,
+    EvaluateRes,
+    FitIns,
+    FitRes,
+    Parameters,
+    ReconnectIns,
+    Scalar,
+)
+from flwr.common.logger import log
+from flwr.common.typing import GetParametersIns
+from flwr.common import parameters_to_ndarrays, ndarrays_to_parameters
+from flwr.server.client_manager import ClientManager
+from flwr.server.client_proxy import ClientProxy
+from flwr.server.history import History
+from flwr.server.strategy import FedAvg, Strategy
+import flwr.server.strategy.aggregate as agg
+from flwr.server.server import Server
+
+from flower_async.async_client_manager import AsyncClientManager
+from flower_async.async_strategy import AsynchronousStrategy
+
+FitResultsAndFailures = Tuple[
+    List[Tuple[ClientProxy, FitRes]],
+    List[Union[Tuple[ClientProxy, FitRes], BaseException]],
+]
+EvaluateResultsAndFailures = Tuple[
+    List[Tuple[ClientProxy, EvaluateRes]],
+    List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]],
+]
+ReconnectResultsAndFailures = Tuple[
+    List[Tuple[ClientProxy, DisconnectRes]],
+    List[Union[Tuple[ClientProxy, DisconnectRes], BaseException]],
+]
+
+
+class AsyncServer(Server):
+    """Flower server."""
+
+    def __init__(
+        self,
+        *args,
+        strategy: Strategy,
+        client_manager: AsyncClientManager,
+        async_strategy: AsynchronousStrategy,
+    ):
+        self.async_strategy = async_strategy
+        super().__init__(*args, strategy=strategy, client_manager=client_manager)
+
+    def set_new_params(self, new_params: Parameters):
+        lock = Lock()
+        with lock:
+            self.parameters = new_params
+        
+
+    # pylint: disable=too-many-locals
+    def fit(self, num_rounds: int, timeout: Optional[float]) -> History:
+        """Run federated averaging for a number of rounds."""
+        history = History()
+
+        # Initialize parameters
+        log(INFO, "Initializing global parameters")
+        self.parameters = self._get_initial_parameters(timeout=timeout)
+        log(INFO, "Evaluating initial parameters")
+        res = self.strategy.evaluate(0, parameters=self.parameters)
+        if res is not None:
+            log(
+                INFO,
+                "initial parameters (loss, other metrics): %s, %s",
+                res[0],
+                res[1],
+            )
+            history.add_loss_centralized(server_round=0, loss=res[0])
+            history.add_metrics_centralized(server_round=0, metrics=res[1])
+
+        # Run federated learning for num_rounds
+        log(INFO, "FL starting")
+        start_time = timeit.default_timer()
+        training_time = 15 # make adjustable via object attribute
+        current_round = 0
+        executor = ThreadPoolExecutor(max_workers=self.max_workers)
+        while timeit.default_timer() - start_time < training_time:
+
+            # Train model and replace previous global model
+            self.fit_round(
+                server_round=current_round,
+                timeout=timeout,
+                executor=executor
+            )
+
+            # Evaluate model using strategy implementation
+            
+            # print("Starting to sleep")
+            # sleep(1)
+            # print("Woke up (turn my swag on)") 
+            # current_round += 1 # No rounds.
+        
+        log(INFO, "FL finished")
+        res_cen = self.strategy.evaluate(current_round, parameters=self.parameters)
+        if res_cen is not None:
+            loss_cen, metrics_cen = res_cen
+            log(
+                INFO,
+                "fit progress: (%s, %s, %s, %s)",
+                current_round,
+                loss_cen,
+                metrics_cen,
+                timeit.default_timer() - start_time,
+            )
+            history.add_loss_centralized(server_round=current_round, loss=loss_cen)
+            history.add_metrics_centralized(
+                server_round=current_round, metrics=metrics_cen
+            )
+
+        # Evaluate model on a sample of available clients
+        res_fed = self.evaluate_round(server_round=current_round, timeout=timeout)
+        if res_fed is not None:
+            loss_fed, evaluate_metrics_fed, _ = res_fed
+            if loss_fed is not None:
+                history.add_loss_distributed(
+                    server_round=current_round, loss=loss_fed
+                )
+                history.add_metrics_distributed(
+                    server_round=current_round, metrics=evaluate_metrics_fed
+                )
+        # Bookkeeping
+        end_time = timeit.default_timer()
+        elapsed = end_time - start_time
+        log(INFO, "FL finished in %s", elapsed)
+        return history
+
+    def evaluate_round(
+        self,
+        server_round: int,
+        timeout: Optional[float],
+    ) -> Optional[
+        Tuple[Optional[float], Dict[str, Scalar], EvaluateResultsAndFailures]
+    ]:
+        """Validate current global model on a number of clients."""
+        # Get clients and their respective instructions from strategy
+        client_instructions = self.strategy.configure_evaluate(
+            server_round=server_round,
+            parameters=self.parameters,
+            client_manager=self._client_manager,
+        )
+        if not client_instructions:
+            log(INFO, "evaluate_round %s: no clients selected, cancel", server_round)
+            return None
+        log(
+            DEBUG,
+            "evaluate_round %s: strategy sampled %s clients (out of %s)",
+            server_round,
+            len(client_instructions),
+            self._client_manager.num_available(),
+        )
+
+        # Collect `evaluate` results from all clients participating in this round
+        results, failures = evaluate_clients(
+            client_instructions,
+            max_workers=self.max_workers,
+            timeout=timeout,
+        )
+        log(
+            DEBUG,
+            "evaluate_round %s received %s results and %s failures",
+            server_round,
+            len(results),
+            len(failures),
+        )
+
+        # Aggregate the evaluation results
+        aggregated_result: Tuple[
+            Optional[float],
+            Dict[str, Scalar],
+        ] = self.strategy.aggregate_evaluate(server_round, results, failures)
+
+        loss_aggregated, metrics_aggregated = aggregated_result
+        return loss_aggregated, metrics_aggregated, (results, failures)
+
+    def fit_round(
+        self,
+        server_round: int,
+        timeout: Optional[float],
+        executor: ThreadPoolExecutor
+    ): #-> Optional[Tuple[Optional[Parameters], Dict[str, Scalar], FitResultsAndFailures]]:
+        """Perform a single round of federated averaging."""
+        # Get clients and their respective instructions from strategy
+        client_instructions = self.strategy.configure_fit(
+            server_round=server_round,
+            parameters=self.parameters,
+            client_manager=self._client_manager,
+        )
+
+        if not client_instructions:
+            log(INFO, "fit_round %s: no clients selected, cancel", server_round)
+            return None
+        log(
+            DEBUG,
+            "fit_round %s: strategy sampled %s clients (out of %s)",
+            server_round,
+            len(client_instructions),
+            self._client_manager.num_available(),
+        )
+
+        # Collect `fit` results from all clients participating in this round
+        fit_clients(
+            client_instructions=client_instructions,
+            timeout=timeout,
+            server=self,
+            executor=executor
+        )
+        
+
+    def disconnect_all_clients(self, timeout: Optional[float]) -> None:
+        """Send shutdown signal to all clients."""
+        all_clients = self._client_manager.all()
+        clients = [all_clients[k] for k in all_clients.keys()]
+        instruction = ReconnectIns(seconds=None)
+        client_instructions = [(client_proxy, instruction) for client_proxy in clients]
+        _ = reconnect_clients(
+            client_instructions=client_instructions,
+            max_workers=self.max_workers,
+            timeout=timeout,
+        )
+
+
+    def _get_initial_parameters(self, timeout: Optional[float]) -> Parameters:
+        """Get initial parameters from one of the available clients."""
+        # Server-side parameter initialization
+        parameters: Optional[Parameters] = self.strategy.initialize_parameters(
+            client_manager=self._client_manager
+        )
+        if parameters is not None:
+            log(INFO, "Using initial parameters provided by strategy")
+            return parameters
+
+        # Get initial parameters from one of the clients
+        log(INFO, "Requesting initial parameters from one random client")
+        random_client = self._client_manager.sample(1)[0]
+        ins = GetParametersIns(config={})
+        get_parameters_res = random_client.get_parameters(ins=ins, timeout=timeout)
+        log(INFO, "Received initial parameters from one random client")
+        return get_parameters_res.parameters
+
+
+def reconnect_clients(
+    client_instructions: List[Tuple[ClientProxy, ReconnectIns]],
+    max_workers: Optional[int],
+    timeout: Optional[float],
+) -> ReconnectResultsAndFailures:
+    """Instruct clients to disconnect and never reconnect."""
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        
+        submitted_fs = {
+            executor.submit(reconnect_client, client_proxy, ins, timeout)
+            for client_proxy, ins in client_instructions
+        }
+        finished_fs, _ = concurrent.futures.wait(
+            fs=submitted_fs,
+            timeout=None,  # Handled in the respective communication stack
+        )
+
+    # Gather results
+    results: List[Tuple[ClientProxy, DisconnectRes]] = []
+    failures: List[Union[Tuple[ClientProxy, DisconnectRes], BaseException]] = []
+    for future in finished_fs:
+        failure = future.exception()
+        if failure is not None:
+            failures.append(failure)
+        else:
+            result = future.result()
+            results.append(result)
+    return results, failures
+
+
+def reconnect_client(
+    client: ClientProxy,
+    reconnect: ReconnectIns,
+    timeout: Optional[float],
+) -> Tuple[ClientProxy, DisconnectRes]:
+    """Instruct client to disconnect and (optionally) reconnect later."""
+    disconnect = client.reconnect(
+        reconnect,
+        timeout=timeout,
+    )
+    return client, disconnect
+
+
+def handle_futures(futures, server):
+    for future in futures:
+        _handle_finished_future_after_fit(
+            future=future,server=server
+        )
+
+def fit_clients(
+    client_instructions: List[Tuple[ClientProxy, FitIns]],
+    timeout: Optional[float],
+    server: AsyncServer,
+    executor: ThreadPoolExecutor
+): #-> FitResultsAndFailures:
+    """Refine parameters concurrently on all selected clients."""
+    #results: List[Tuple[ClientProxy, FitRes]] = []
+    #failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]] = []
+    
+    submitted_fs = {
+        executor.submit(fit_client, client_proxy, ins, timeout)
+        for client_proxy, ins in client_instructions
+    }
+    for f in submitted_fs:      
+       f.add_done_callback(
+            lambda ftr: _handle_finished_future_after_fit(ftr, server=server),            
+        )
+
+    
+    # finished_fs = concurrent.futures.as_completed(submitted_fs)
+    # executor.submit(
+    #     handle_futures, finished_fs, server
+    # )
+
+
+
+    
+
+def fit_client(
+    client: ClientProxy, ins: FitIns, timeout: Optional[float]
+) -> Tuple[ClientProxy, FitRes]:
+    """Refine parameters on a single client."""
+    fit_res = client.fit(ins, timeout=timeout)
+    return client, fit_res
+
+
+def _handle_finished_future_after_fit(
+    future: concurrent.futures.Future,  # type: ignore
+    #results: List[Tuple[ClientProxy, FitRes]],
+    #failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
+    server: AsyncServer
+) -> None:
+    """Convert finished future into either a result or a failure."""
+    # Check if there was an exception
+     
+    failure = future.exception()
+    if failure is not None:
+        print("Got a failure :(")
+        
+        #failures.append(failure)
+        return
+    print("Got a result :)")
+    # Successfully received a result from a client
+    result: Tuple[ClientProxy, FitRes] = future.result()
+    clientProxy, res = result
+
+    server.client_manager().set_client_to_free(clientProxy.cid)
+    
+    # Check result status code
+    if res.status.code == Code.OK:
+        # Merge into global model.
+        # aggregated_result: Parameters = ndarrays_to_parameters(agg.aggregate([ ( parameters_to_ndarrays(res.parameters), 1) , 
+        #                                                (parameters_to_ndarrays(server.parameters), 1) ])) # 1 is the number of samples and here we just take the average of the parameters (TODO: add staleness and num examples weighing)
+        # parameters_aggregated = aggregated_result
+        # parameters_aggregated = server.async_strategy.weighted_average_fedasync(server.parameters, res.parameters, res.metrics['t_diff'])
+        parameters_aggregated = server.async_strategy.average(server.parameters, res.parameters, res.metrics['t_diff'], res.num_examples)
+        server.set_new_params(parameters_aggregated)
+        return
+    
+    # Not successful, client returned a result where the status code is not OK
+    #failures.append(result)
+
+############################### FOR EVALUATION ####################################
+
+def evaluate_clients(
+    client_instructions: List[Tuple[ClientProxy, EvaluateIns]],
+    max_workers: Optional[int],
+    timeout: Optional[float],
+) -> EvaluateResultsAndFailures:
+    """Evaluate parameters concurrently on all selected clients."""
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        submitted_fs = {
+            executor.submit(evaluate_client, client_proxy, ins, timeout)
+            for client_proxy, ins in client_instructions
+        }
+        finished_fs, _ = concurrent.futures.wait(
+            fs=submitted_fs,
+            timeout=None,  # Handled in the respective communication stack
+        )
+
+    # Gather results
+    results: List[Tuple[ClientProxy, EvaluateRes]] = []
+    failures: List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]] = []
+    for future in finished_fs:
+        _handle_finished_future_after_evaluate(
+            future=future, results=results, failures=failures
+        )
+    return results, failures
+
+
+def evaluate_client(
+    client: ClientProxy,
+    ins: EvaluateIns,
+    timeout: Optional[float],
+) -> Tuple[ClientProxy, EvaluateRes]:
+    """Evaluate parameters on a single client."""
+    evaluate_res = client.evaluate(ins, timeout=timeout)
+    return client, evaluate_res
+
+
+def _handle_finished_future_after_evaluate(
+    future: concurrent.futures.Future,  # type: ignore
+    results: List[Tuple[ClientProxy, EvaluateRes]],
+    failures: List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]],
+) -> None:
+    """Convert finished future into either a result or a failure."""
+    # Check if there was an exception
+    failure = future.exception()
+    if failure is not None:
+        failures.append(failure)
+        return
+
+    # Successfully received a result from a client
+    result: Tuple[ClientProxy, EvaluateRes] = future.result()
+    _, res = result
+
+    # Check result status code
+    if res.status.code == Code.OK:
+        results.append(result)
+        return
+
+    # Not successful, client returned a result where the status code is not OK
+    failures.append(result)

9e29f5508b199ec1842ddeef10bcce3439c12abf
r-gg <gajaninrastko@gmail.com>
2024-07-03 20:29:35 +0200

    Adding the send gradients option

--- async_strategy.py
+++ async_strategy.py
@@ -10,10 +10,9 @@ from logging import DEBUG, WARNING
 class AsynchronousStrategy:
     """Abstract base class for all asynchronous strategies."""
 
-    def __init__(self, total_samples: int, alpha: float, staleness_alpha: float, fedasync_mixing_alpha: float, fedasync_a: float, num_clients: int, async_aggregation_strategy: str,
-                 use_staleness: bool, use_sample_weighing: bool) -> None:
+    def __init__(self, total_samples: int, staleness_alpha: float, fedasync_mixing_alpha: float, fedasync_a: float, num_clients: int, async_aggregation_strategy: str,
+                 use_staleness: bool, use_sample_weighing: bool, send_gradients: bool) -> None:
         self.total_samples = total_samples
-        self.alpha = alpha  # aggregation alpha (FedAsynch)
         self.staleness_alpha = staleness_alpha
         self.fedasync_a = fedasync_a
         self.fedasync_mixing_alpha = fedasync_mixing_alpha
@@ -21,15 +20,19 @@ class AsynchronousStrategy:
         self.async_aggregation_strategy = async_aggregation_strategy
         self.use_staleness = use_staleness
         self.use_sample_weighing = use_sample_weighing
+        self.send_gradients = send_gradients
 
     def average(self, global_parameters: Parameters, model_update_parameters: Parameters, t_diff: float, num_samples: int) -> Parameters:
         """Compute the average of the global and client parameters."""
         if self.async_aggregation_strategy == "fedasync":
-            return self.weighted_average_fedasync(global_parameters, model_update_parameters, t_diff, num_samples)
-        elif self.async_aggregation_strategy == "asyncfeded":
-            return self.weighted_average_asyncfeded(global_parameters, model_update_parameters, t_diff, num_samples)
-        elif self.async_aggregation_strategy == "unweighted":
-            return self.unweighted_average(global_parameters, model_update_parameters)
+            if self.send_gradients:
+                return self.weighted_merge_fedasync(global_parameters, model_update_parameters, t_diff, num_samples)
+            else:
+                return self.weighted_average_fedasync(global_parameters, model_update_parameters, t_diff, num_samples)
+        # elif self.async_aggregation_strategy == "asyncfeded":
+        #     return self.weighted_average_asyncfeded(global_parameters, model_update_parameters, t_diff, num_samples)
+        # elif self.async_aggregation_strategy == "unweighted":
+        #     return self.unweighted_average(global_parameters, model_update_parameters)
         else:
             raise ValueError(
                 f"Invalid async aggregation strategy: {self.async_aggregation_strategy}")
@@ -64,6 +67,26 @@ class AsynchronousStrategy:
         # log(DEBUG, f"t_diff: {t_diff}\nalpha_coeff: {alpha_coeff}")
 
         return [(1 - alpha_coeff) * layer_global + alpha_coeff * layer_update for layer_global, layer_update in zip(global_param_arr, model_update_param_arr)]
+
+    def weighted_merge_fedasync(self, global_parameters: Parameters, gradients: Parameters, t_diff: float, num_samples: int) -> Parameters:
+        """Add gradients to the global model. Inspired by the paper Fedasync : https://arxiv.org/pdf/1903.03934.pdf
+        It is not however the same procedure as in original paper, because they aggregate MODELS and we aggregate GRADIENTS.
+        """
+        return ndarrays_to_parameters(self.add_grads_fedasync(parameters_to_ndarrays(global_parameters), parameters_to_ndarrays(gradients), t_diff, num_samples=num_samples))
+
+    def add_grads_fedasync(self, global_param_arr: NDArrays, gradients_arr: NDArrays, t_diff: float, num_samples: int) -> NDArrays:
+        """Compute weighted average with the formula params_new = (1-alpha) * params_old + alpha * (params_old + update_grads)"""
+        # Calculate the total number of examples used during training
+        alpha_coeff = self.fedasync_mixing_alpha
+        if self.use_staleness:
+            alpha_coeff *= self.get_staleness_weight_coeff_fedasync_poly(
+                t_diff=t_diff)
+        if self.use_sample_weighing:
+            alpha_coeff *= self.get_sample_weight_coeff(num_samples)
+
+        # log(DEBUG, f"t_diff: {t_diff}\nalpha_coeff: {alpha_coeff}")
+
+        return [(1 - alpha_coeff) * layer_global + alpha_coeff * (layer_global + layer_grad) for layer_global, layer_grad in zip(global_param_arr, gradients_arr)] 
 
     # See paper: https://arxiv.org/pdf/2205.13797.pdf
     def aggregate_asyncfeded(self, global_param_arr: NDArrays, model_update_param_arr: NDArrays, t_diff: float, num_samples: int) -> NDArrays:
